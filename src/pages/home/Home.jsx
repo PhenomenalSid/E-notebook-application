05c1220ac5de7d38fc45dcd96f0f546279e2c269
Siddharth Rawat <siddharth.rawat0305@gmail.com>
2024-03-01 09:28:56 +0530

    first commit

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import NotesCard from "../../components/notecard/NoteCard";
+import Layout from "../../components/layout/Layout";
+
+function Home() {
+  return (
+    <Layout>
+      <NotesCard />
+    </Layout>
+  );
+}
+
+export default Home;
